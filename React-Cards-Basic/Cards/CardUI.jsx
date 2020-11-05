5551e4912fe661e65b12bf6bb94693bc6657b7e1
Appearing Technology <73649106+appearingtech@users.noreply.github.com>
2020-11-05 21:13:27 +0530

    Create CardUI.jsx

---
+++ React-Cards-Basic/Cards/CardUI.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./card-style.css";
+ 
+const Card = props => {
+  return (
+    <div className='card text-center shadow'>
+      <div className='overflow'>
+        <img src={props.imgsrc} alt='Image 1' className='card-img-top' />
+      </div>
+      <div className='card-body text-dark'>
+        <h4 className='card-title'>{props.title}</h4>
+        <p className='card-text text-secondary'>
+        Artificial intelligence (AI) refers to the simulation of human intelligence in machines that are programmed to think like humans and mimic their actions.
+        </p>
+        <a href='#' className='btn btn-outline-success'>
+          Go Anywhere
+        </a>
+      </div>
+    </div>
+  );
+};
+ 
+export default Card;
